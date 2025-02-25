a3a7d5c4b73055351366c85f61672671381a3e75
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2025-02-25 03:07:38 +0100

    Delete all directory

--- all/CBS.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "map.h"
-#include "compute.h"
-
-namespace internal {
-    void resolveConflictsCBS(AlgorithmType which, Map& m, CTNode& root, int numThreads, std::vector<std::vector<Position>>& solution);
-}
--- all/move_agent.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "agent.h"
-#include "map.h"
-
-
-namespace internal {
-    inline void moveAgentForIndex(int id, MemoryPointers& localMemory);
-}
